debbe782d27b05c420187cbfb00d92f120517688
Kris Mulyandani Putra <kris.mp7777@gmail.com>
2018-05-20 16:20:43 +0700

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  print('Hello wkwkwk');
+  runApp(
+    MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Hello Rectangle')
+        ),
+        body: Container(
+          color: Colors.greenAccent
+        ),
+      ),
+    ),
+  );
+}
